9b19e70bcf1cc3ef7fbfebe3c65247bc68e066a3
Tamil-3 <155735628+Tamil-3@users.noreply.github.com>
2024-02-13 17:23:12 +0530

    Add files via upload

--- loops.py
+++ loops.py
@@ -3,6 +3,48 @@
 # range(start, stop, step)
 # for i in range(3, 11, 2):
 #     print(i)
-list = [2, 4, 6, 8]
-for x in list:
-    print(x)
+'''
+sum = 0
+for x in range(2, 11, 2):
+    sum = sum + x
+print(sum)
+'''
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 1
+'''
+num = int(input("Enter a number: "))
+i = 1
+while i <= 10:
+    res = i * num
+    print(res)
+    i += 1
+'''
+# Nested Loops
+'''
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(j, end=' ')
+    print()
+'''
+
+for i in range(1, 10):
+    if(i == 5):
+        # break
+        continue
+    print(i)
+print("Program end...")
+
+
+
+
+
+
+
+
+
+
+
+
+
