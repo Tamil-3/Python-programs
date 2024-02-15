41a3e032847131b975d1f886684f5cb04a29c728
Tamil-3 <155735628+Tamil-3@users.noreply.github.com>
2024-02-15 17:46:04 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,31 @@
+'''
+    def func_name(): // function definition
+    func_name()  // function calling
+'''
+
+# def greet():
+#     print("Hello World")
+#
+# greet()
+
+# def message(name):
+#     print(f"Hello {name}, Welcome to Python")
+#
+# message("Kumar")
+
+def showInfo():
+    emp_name = "Raj"
+    emp_id = 101
+    return [emp_name,emp_id]
+
+details = showInfo()
+print(details)
+
+'''
+def add(n1, n2):
+    res = n1 + n2
+    return res
+
+sum = add(10, 3)
+print(sum)
+'''
