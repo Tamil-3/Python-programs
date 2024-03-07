d4a139e4f2c9384a5e6f5fa1919fb36b6a85fdf6
Tamil-3 <155735628+Tamil-3@users.noreply.github.com>
2024-03-07 17:39:37 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,41 +1,11 @@
 '''
-    543544 -> 5 => 4
-    abc123 -> 3
-
-    num = 543544
-n = 5
-count = 0
-
-for i in str(num):
-    if i != str(n):
-        count += 1
-print(count)
-
-str1 = "a1b2c3123"
-count = 0
-
-for i in str1:
-    if i >= '0' and i <= '9':
-        count += 1
-print(count)
+    2 5
+    2 ^ 5 -> 2 * 2 * 2 * 2 * 2
 '''
 
-
-list1 = []
-no_of_input = int(input("Enter a no of input: "))
-
-for i in range(no_of_input):
-    num = int(input())
-    list1.append(num)
-print(list1)
-
-
-
-
-
-
-
-
-
-
-
+base = int(input())
+power = int(input())
+res = 1
+for i in range(1, power + 1):
+    res = res * base
+print(res)
